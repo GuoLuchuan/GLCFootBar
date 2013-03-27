970084c15aa8adf46b9fcac2696db1e174fe44e6
GuoLuchuan <guoluchuan@gmail.com>
2013-03-27 14:20:56 +0800

    Opitimization the effect of the demo

---
+++ GLCFootBar/GLCBaseNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  GLCBaseNavigationController.h
+//  GLCFootBar
+//
+//  Created by guo luchuan on 13-3-27.
+//  Copyright (c) 2013年 yours. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLCBaseNavigationController : UINavigationController
+
+@end
---
+++ GLCFootBar/GLCBaseNavigationController.m
@@ -0,0 +1,38 @@
+//
+//  GLCBaseNavigationController.m
+//  GLCFootBar
+//
+//  Created by guo luchuan on 13-3-27.
+//  Copyright (c) 2013年 yours. All rights reserved.
+//
+
+#import "GLCBaseNavigationController.h"
+
+@interface GLCBaseNavigationController ()
+
+@end
+
+@implementation GLCBaseNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
